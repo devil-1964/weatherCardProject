f8e70e04b80bb3f6f17a34c95ff3ca694ace646d
devil-1964 <devgoe5115@gmail.com>
2023-10-25 19:53:48 +0530

    Complete whole project

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("input");
+  const city = document.getElementById("city");
+  const degree = document.getElementById("degree");
+  const humidity = document.getElementById("humidity");
+  const windSpeed = document.getElementById("windSpeed");
+
+  input.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      fetchData(input.value)
+        .then((data) => {
+          city.innerHTML = data.location.name;
+          degree.innerHTML = data.current.temp_c;
+          humidity.innerHTML = data.current.humidity;
+          windSpeed.innerHTML = data.current.wind_kph;
+        })
+        .catch((error) => {
+          alert("Wrong Input");
+        });
+    }
+  });
+});
+
+function onBtn() {
+  fetchData(input.value)
+    .then((data) => {
+      city.innerHTML = data.location.name;
+      degree.innerHTML = data.current.temp_c;
+      humidity.innerHTML = data.current.humidity;
+      windSpeed.innerHTML = data.current.wind_kph;
+    })
+    .catch((error) => {
+      alert("Wrong Input");
+    });
+}
+
+const fetchData = async (value) => {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=7b0f965e60db490c8f1113518232510&api=yes&q=${value}`
+    );
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        black:"black",
+        white:"white",
+        red:"red",
+        peach:"#FCEDDA",
+        burntOrange:"#EE4E34"
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
